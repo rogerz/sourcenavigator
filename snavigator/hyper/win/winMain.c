be04287feb02895ca1eddba906062fee03e730ba
mdejong <mdejong@598d5b88-e02c-0410-8697-e0f804853bb7>
2002-04-20 00:32:51 +0000

    * configure.in: Avoid setting HYPER_CONSOLE. 	* hyper/Makefile.am: Avoid using HYPER_CONSOLE 	when linking hyper.exe. 	* hyper/Makefile.in: Ditto. 	* hyper/win/winMain.c (WinMain, Tcl_AppInit): 	Use public Tk_CreateConsoleWindow instead of 	internal TkConsoleCreate and TkConsoleInit 	functions. This removes the need to link 	to the HYPER_CONSOLE object from tk which 	should make it easier to build with an 	already installed version of Tk.

--- snavigator/hyper/win/winMain.c
+++ snavigator/hyper/win/winMain.c
@@ -53,14 +53,6 @@ MA 02111-1307, USA.
 #include "mxfuncs.h"
 
 /*
- * The following declarations refer to internal Tk routines.  These
- * interfaces are available for use, but are not supported.
- */
-
-EXTERN void		TkConsoleCreate _ANSI_ARGS_((void));
-EXTERN int		TkConsoleInit _ANSI_ARGS_((Tcl_Interp *interp));
-
-/*
  * Forward declarations for procedures defined later in this file:
  */
 
@@ -194,15 +186,13 @@ int main(int argc2,char **argv2)
 
     /*
      * Create the console channels and install them as the standard
-     * channels.  All I/O will be discarded until TkConsoleInit is
+     * channels.  All I/O will be discarded until Tcl_CreateConsoleWindow is
      * called to attach the console to a text widget.
      */
 
     useWinConsole = TkpWinConsoleTest();
     if ( useWinConsole ) {
         TkpWinConsoleCreate();
-    } else {
-        TkConsoleCreate();
     }
 
     /*
@@ -414,7 +404,7 @@ Tcl_AppInit(interp)
 
     if ((strcmp(Tcl_GetVar(interp, "tcl_interactive", TCL_GLOBAL_ONLY), "1")
 	    == 0) || ( ! useWinConsole )) {
-	if (TkConsoleInit(interp) == TCL_ERROR) {
+	if (Tk_CreateConsoleWindow(interp) == TCL_ERROR) {
 	goto error;
 	}
 #ifdef WIN32
