1e1f155c624001aabfcd47b604940e5ee7f773fc
mdejong <mdejong@598d5b88-e02c-0410-8697-e0f804853bb7>
2003-05-06 20:40:08 +0000

    * parsers/m4/m4browser.c: Regen. 	* parsers/m4/m4browser.l: Fix quoted-symbol 	define so that "SYMBOL" and "[SYMBOL]" are 	accepted but "[SYMBOL" and "SYMBOL]" are not. 	Fix dnl comment rule so that a line that 	begins with "dnl TEXT" is accepted but 	"dnlTEXT" is not. 	* tests/parsers/m4.test: Add basic set of 	regression tests for the m4 parser. 	* tests/parsers/testutil.tcl: Add helper 	utils that are used to test parsers.

---
+++ snavigator/tests/parsers/testutil.tcl
@@ -0,0 +1,96 @@
+package require tcltest
+
+proc save_file { fname data {encoding default} } {
+    set fd [open $fname w]
+    if {$encoding != "default"} {
+        fconfigure $fd -encoding $encoding
+    }
+    puts -nonewline $fd $data
+    close $fd
+    return $fname
+}
+
+# Read data from file in utf-8 format.
+# Pass "default" to use the system encoding.
+
+proc read_file { fname {encoding utf-8} } {
+    set fd [open $fname r]
+    if {$encoding != "default"} {
+        fconfigure $fd -encoding $encoding
+    }
+    set data [read -nonewline $fd]
+    close $fd
+    return $data
+}
+
+# Return string for id, see sn.h for declarations
+
+proc paf_get { id } {
+    array set paf_values {
+0  PAF_FILE
+1  PAF_TYPE_DEF
+2  PAF_CLASS_DEF
+3  PAF_MBR_FUNC_DEF
+4  PAF_MBR_VAR_DEF
+5  PAF_ENUM_DEF
+6  PAF_CONS_DEF
+7  PAF_MACRO_DEF
+8  PAF_FUNC_DEF
+9  PAF_SUBR_DEF
+10 PAF_GLOB_VAR_DEF
+11 PAF_COMMON_DEF
+12 PAF_COMMON_MBR_VAR_DEF
+13 PAF_CLASS_INHERIT
+14 PAF_FILE_SYMBOLS
+15 PAF_CROSS_REF_BY
+16 PAF_CROSS_REF
+17 PAF_MBR_FUNC_DCL
+18 PAF_FUNC_DCL
+19 PAF_ENUM_CONST_DEF
+20 PAF_UNION_DEF
+21 PAF_FRIEND_DCL
+22 PAF_NAMESPACE_DEF
+23 PAF_EXCEPTION_DEF
+24 PAF_LOCAL_VAR_DEF
+25 PAF_VAR_DCL
+26 PAF_INCLUDE_DEF
+27 PAF_COMMENT_DEF
+28 PAF_CROSS_REF_CPP
+29 PAF_REF_UNDEFINED
+30 PAF_CROSS_REF_FILE
+    }
+
+    if {![info exists paf_values($id)]} {
+        return "UNKNOWN"
+    } else {
+        return $paf_values($id)
+    }
+}
+
+# Scan output from a parser and subst paf string
+# values into lines that start with a number
+# followed by a semicolon.
+
+proc paf_subst { data } {
+    set buf ""
+    foreach line [split $data \n] {
+        if {[regexp {^([0-9]+);(.*)$} $line whole num rest]} {
+            set paf_str [paf_get $num]
+            append buf $paf_str\;$rest\n
+        } else {
+            append buf $line\n
+        }
+    }
+    return [string map {\1 <>} $buf]
+}
+
+proc m4_parse { fname } {
+    return [paf_subst [exec m4browser $fname]]
+}
+
+proc m4_parse_xref { fname } {
+    file delete xout
+    save_file xout ""
+    set r [exec m4browser -x xout $fname]
+    return [paf_subst [read_file xout]]
+}
