6d13ad4a1a1f35e2179d98d3a42df22c94f894ff
freequaos <freequaos@598d5b88-e02c-0410-8697-e0f804853bb7>
2008-06-12 17:50:41 +0000

    (ENHANCE) Change programm structure for fsstnd compliance, part II o introduce sn_dir(libexecdir) in paths.tcl (for parsers, etc)

--- snavigator/gui/paths.tcl
+++ snavigator/gui/paths.tcl
@@ -36,6 +36,7 @@ proc sn_initialize_paths {} {
     set sn_path(bitmapdir) [file join ${sn_home} share bitmaps]
     set sn_path(rundir) [file join ${sn_home} run]
     set sn_path(etcdir) [file join ${sn_home} share etc]
+    set sn_path(libexecdir) [file join ${sn_home} libexec]
     set sn_path(toolchaindir) [file join ${sn_home} share etc sn_toolchains]
 }
 
