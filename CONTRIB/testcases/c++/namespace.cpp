5d0636a8bd2ad2063604825fbed16e4892a9e03c
freequaos <freequaos@598d5b88-e02c-0410-8697-e0f804853bb7>
2011-11-07 19:05:50 +0100

    (ENHANCE) Add testcase for C++ namespaces into CONTRIB/testcases/c++ , part II o add a 3rd namespace to check against namespace parser problems

--- CONTRIB/testcases/c++/namespace.cpp
+++ CONTRIB/testcases/c++/namespace.cpp
@@ -19,12 +19,21 @@ namespace test2 {
         }
 }
 
+namespace test3 {
+        int func(bool z)
+        {
+                return z&1;
+        }
+
+}
+
 int main(int ac, char **dc)
 {
         using namespace std;
 
         cout << "test1:: " << test1::func(true) << endl;
         cout << "test2:: " << test2::func(true) << endl;
+        cout << "test3:: " << test3::func(true) << endl;
 
         return 0;
 }
